abc0a1bc2f18c32738eebc1979be02b30f11d832
Anthony Merlier <46730440+ideosconsor@users.noreply.github.com>
2021-07-11 10:39:32 +0200

    video 3 ok - webpack + sass & js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,11 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>My App</title>
+    <link rel="stylesheet" href="css/app.css">
 </head>
 <body>
     @include('partials.header')
     
     @yield('content')
 
+    <script src="js/app.js"></script>
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Welcome to my Application</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ex fuga harum nihil officia temporibus vero similique nulla, quaerat inventore necessitatibus optio provident adipisci maiores quisquam quod illo non. Ducimus doloribus accusantium incidunt, magnam inventore assumenda dolorum voluptatum eius eum.</p>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit eius deleniti commodi rerum minima tempora iusto ipsam, quisquam animi assumenda?</p>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>Welcome to my Application</h1>
+    <p class="text-danger">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ex fuga harum nihil officia temporibus vero similique nulla, quaerat inventore necessitatibus optio provident adipisci maiores quisquam quod illo non. Ducimus doloribus accusantium incidunt, magnam inventore assumenda dolorum voluptatum eius eum.</p>
+    <p class="fw-bold">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit eius deleniti commodi rerum minima tempora iusto ipsam, quisquam animi assumenda?</p>
+            </div>
+        </div>
+    </div>
 @endsection
